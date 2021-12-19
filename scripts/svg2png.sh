d30d690ebb94ab2769d831f4fb4b14551aac19f0
ntak1 <naomitkm1@gmail.com>
2021-12-19 18:47:47 -0300

    Minor update in convert script

--- scripts/svg2png.sh
+++ scripts/svg2png.sh
@@ -3,12 +3,12 @@
 # This script converts .svg files into png using image magik
 # 1. Copy this to the folder containing the .svg files
 # 2. Run this script
-
-ls | grep "svg" > files.txt
+BASE_DIR="../shogi-pieces/kanji_light/"
+ls $BASE_DIR | grep "svg" > files.txt
 while IFS= read -r line; do
     echo "Text read from file: $line"
-	mkdir -p png
-	new_name=`echo $line | cut -f1 -d"."`
+	mkdir -p $BASE_DIR/png
+	new_name=`echo $line | cut -f1 -d"."`.png
 	echo $new_name
-	convert $line png/$new_name
+	convert $BASE_DIR/$line $BASE_DIR/png/$new_name
 done < files.txt
